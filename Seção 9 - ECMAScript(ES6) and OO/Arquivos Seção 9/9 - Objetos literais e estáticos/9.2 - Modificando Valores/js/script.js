58d5ea3847fac7b9c59808a79f45d7e452f7d0a2
Dhionathan Jobim <dhionathan01@hotmail.com>
2022-02-08 16:20:47 -0300

    9 -  Objetos litetais : Modificando Valores

---
+++ Seção 9 - ECMAScript(ES6) and OO/Arquivos Seção 9/9 - Objetos literais e estáticos/9.2 - Modificando Valores/js/script.js
@@ -0,0 +1,13 @@
+// Objetos literais : Modificando valores
+let pessoa = {
+    nome: 'Jose',
+    idade: 45
+}
+
+console.log(pessoa.nome)
+// Modificando os valores dos objetos
+pessoa.nome = 'Fernanda'
+pessoa.idade = 32
+
+console.log(pessoa.nome)
+console.log(pessoa.idade)
