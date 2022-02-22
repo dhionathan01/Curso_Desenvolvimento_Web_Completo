f9cd69d925679e5704681c063f53f8b5889d7fe1
Dhionathan Jobim <dhionathan01@hotmail.com>
2022-02-22 16:08:45 -0300

    20 - Destructuring Assignment em Rest

---
+++ Seção 9 - ECMAScript(ES6) and OO/Arquivos Seção 9/20 - Destructuring Assignment parte 4 = Rest/scripts.js
@@ -0,0 +1,16 @@
+//array
+/* 
+let arr = [10, 20, 30, 40]
+let [a, ...resto] = arr
+
+console.log(a)
+console.log(resto) 
+*/
+
+let obj = {
+    a: 10, b: 20, c: 30, d: 40
+}
+
+let { a, ...z } = obj
+console.log(a)
+console.log(z)
